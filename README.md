a7e79c388dacbfd440c11aea43b952677b7e2b34
coburnw <rcw@peak.org>
2022-12-15 14:50:24 -0800

    minor cleanup

--- README.md
+++ README.md
@@ -3,18 +3,23 @@ Python Library to Plot Time Series Data Points in Real Time using Tkinter
 
 An attempt to simplify plotting a time series in Python. This repository's only dependancy is the Python native Tkinter library. 
 
-To use on a headless raspberry pi zero with an OS Lite install:
+To use on a headless Raspberry Pi Zero with an OS Lite install:
  * ``ssh -X pi@mypi.local``
  * ``apt install python3-tk``
  * ``git clone https://github.com/coburnw/stripchart``
- * ``python stripchart.py``
+ * ``python chart.py``
  
-The -X option to ssh is the magic that allows a Lite install to delegate Tk's graphics rendering to the clients display manager.
+The -X option of ssh is the magic that allows a Lite install to delegate Tk's graphics rendering to the clients display manager.
 
 Todo:
  * ticks are somewhat erratic
- * x axis is in decades rather than a standard time series
- * more factoring needed to make class interaction more intuitive 
- * raise and catch a dedicated overrange event, in both x and y
-  
+ * x axis scale is in decades rather than a time series
+ * more factoring needed to make class interaction by the developer more intuitive 
+ * raise and catch dedicated overrange events, in both x and y
+ * fix the many omissions
+
+Credit:
+ * Tk Best Practices [https://tkdocs.com/index.html](https://tkdocs.com/index.html)
+ * Enrico's [tkintr](https://github.com/enrico-dibacco/tkinter) repository for the first example that clicked
+ 
 Still very rough. Virtually untested.
--- chart.py
+++ chart.py
@@ -1,8 +1,6 @@
 import tkinter as tk
 import math
 
-#https://github.com/enrico-dibacco/tkinter
-
 class Legend():
     def __init__(self, traces):
         self.traces = traces
@@ -14,18 +12,19 @@ class Legend():
         y = 50 + self.index*10
         for trace in self.traces:
             text = '{} ({})'.format(trace.title, trace.units)
-            chart.create_text(x, y+10*self.index, anchor=tk.SE,
+            chart.create_text(x, y+12*self.index, anchor=tk.SE,
                               text=text, fill=trace.color)
             self.index += 1
         return
         
 class Chart(tk.Canvas):
-    def __init__(self, root, width, height, background_color):
+    def __init__(self, root, width, height, background_color, title=''):
         super().__init__(root, width=width, height=height, bg=background_color)
         self.root = root
         self.width = width
         self.height = height
         self.center = self.height//2
+        self.title = title
 
         self.traces = []
         self.scales = []
@@ -68,48 +67,38 @@ if __name__ == '__main__':
     import trace
     
     class SineSignal(trace.Trace):
-        def __init__(self, x_axis, y_axis, title, initial_value=0,
-                     step=1.0):
+        def __init__(self, x_axis, y_axis, title, initial_value=0, step=1.0/6.28):
             super().__init__(x_axis, y_axis, title)
             self.initial_value = initial_value
             self.step = step
+            self.scale = 1
             self.x = 0
             return
 
         def next_value(self):
-            y = 4*math.sin(self.x+self.initial_value)
+            y = self.scale * math.sin(self.x+self.initial_value)
             x = self.x + self.step
             self.x = x
-            print(x,y)
             return (x,y)
 
-    # for i in range(1, 99, 10):
-    #     i=i/100
-    #     rng = TickRange(-i, i)
-    #     print('i={} max={}e{}'.format(i, rng.tmax.value, rng.tmax.exponent))
-    #     for tick in rng.ticks():
-    #         print('placing tick {}e{}'.format(tick.coefficient, tick.exponent))
-
-    # exit()
-    
     root = tk.Tk()
     root.title('TK StripChart')
     
     chart = Chart(root, 1200, 600, 'white')
     time_axis = axis.HorizontalAxis()
     time_scale = scale.LinearScale(time_axis, position='center', units='Secs')
-    chart.append_scale(time_scale, min=0, max=40)
+    chart.append_scale(time_scale, min=0, max=20)
 
-    volts_axis = axis.VerticalAxis()
-    volts_scale = scale.LinearScale(volts_axis, position='left', units='Volts')
-    chart.append_scale(volts_scale, min=-22, max=22)
+    signal_axis = axis.VerticalAxis()
+    volts_scale = scale.LinearScale(signal_axis, position='left', units='Volts')
+    chart.append_scale(volts_scale, min=-1.5, max=1.5)
     
     sine = SineSignal(time_scale, volts_scale, title='Line')
     chart.append_trace(sine)
-    cosine = SineSignal(time_scale, volts_scale, title='Cross', initial_value=2)
+    cosine = SineSignal(time_scale, volts_scale, title='Cross', initial_value=6.28/4)
     chart.append_trace(cosine)
     
-    chart.update(1000)
+    chart.update(500)
     chart.show()
 
     root.mainloop()
